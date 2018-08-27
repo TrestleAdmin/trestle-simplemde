92c7203b2b230c9bc43cca30525f648c6a3b3b8b
Sam Pohlenz <sam@sampohlenz.com>
2018-08-27 19:28:43 +0930

    Use Trestle.init callback to setup SimpleMDE

--- app/assets/javascripts/trestle/simplemde.js
+++ app/assets/javascripts/trestle/simplemde.js
@@ -2,8 +2,8 @@
 
 Trestle.SimpleMDE = {};
 
-Trestle.ready(function() {
-  $('textarea.simplemde').each(function() {
+Trestle.init(function(e, root) {
+  $('textarea.simplemde', root).each(function() {
     var options = $.extend({}, { element: this }, Trestle.SimpleMDE);
     var simplemde = new SimpleMDE(options);
   });
